4f2e43d1e8665925c02f73ba37be4ed6154795b9
AliRezaBeitari <beitari.alireza@gmail.com>
2019-10-02 01:26:24 +0330

    generate api token in web login if not generated yet

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
 class LoginController extends Controller
 {
     /*
@@ -35,5 +38,13 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function authenticated(Request $request, $user)
+    {
+        if (! $user->api_token) {
+            $user->api_token = Str::random(80);
+            $user->save();
+        }
     }
 }
